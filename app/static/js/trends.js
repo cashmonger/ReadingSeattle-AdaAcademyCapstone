d121f8dfc0a63d5f454117ea978595458a9139f2
Amy Cash <amymcash@gmail.com>
2018-01-25 18:06:38 -0800

    Fix background

--- app/static/js/trends.js
+++ app/static/js/trends.js
@@ -11,7 +11,7 @@ console.log(trendDataset)
 let margin = {top: 30, right: 20, bottom: 20, left: 60}
 let padding = {top: 30, right: 30, bottom: 50, left: 50}
 
-let width = 1100 - margin.left - margin.right; // 1060
+let width = 1000 - margin.left - margin.right; // 1060
 let height = 420 - margin.top - margin.bottom; // 510
 // let padding = 20;
 
